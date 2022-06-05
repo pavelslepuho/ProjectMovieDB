4e344abb42da4ff48645adfbd2f6d47af978f409
PavelSL <spav112@yandex.ru>
2022-06-05 14:42:32 +0300

    cycle and condition added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 console.log(numberOfFilms);
 
-let personaMovieDB = {
+let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -11,12 +11,18 @@ let personaMovieDB = {
     privat: false,
 };
 
-let lastFilm = prompt("Один из последних просмотренных фильмов?"),
-    lastFilmRate = prompt("На сколько оцените его?"),
-    secondLastFilm = prompt("Один из последних просмотренных фильмов?"),
-    secondLastFilmRate = prompt("На сколько оцените его?");
+for (let i = 0; i <= 1; i++) {
+    let lastFilm = prompt("Один из последних просмотренных фильмов?"),
+        lastFilmRate = prompt("На сколько оцените его?");
+        if (lastFilm != null && lastFilmRate != null && lastFilm != '' && lastFilmRate != '' && lastFilm.length < 50 && typeof(lastFilmRate) === 'number' ) {
+            personalMovieDB.movies[lastFilm] = lastFilmRate;
+            console.log('done');
+        } else if (lastFilm === null || lastFilmRate === null) {
+            break;
+        } else {
+            console.log('error');
+            i--;
+        }
+}
 
-personaMovieDB.movies[lastFilm] = lastFilmRate;
-personaMovieDB.movies[secondLastFilm] = secondLastFilmRate;
-
-console.log(personaMovieDB);
+console.log(personalMovieDB);
